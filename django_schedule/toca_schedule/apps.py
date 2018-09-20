5e5603d70dc4fec9c4627392540ec3e51af06281
james-swash <james.swash@hotmail.co.uk>
2018-09-20 11:29:35 +0100

    purge

--- django_schedule/toca_schedule/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TocaScheduleConfig(AppConfig):
-    name = 'toca_schedule'
--- django_schedule/toca_schedule/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class CreateSchedule(forms.ModelForm):
-    class Meta:
-        model = models.Schedule
-        fields = ['job', 'action_id', 'description', 'scheduled']
